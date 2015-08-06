56c7517b3cd99e166957e46a60e65708568773ad
zhangjinfeng <pokit@zhangfengIMac.local>
2015-08-06 18:06:02 +0800

    Initial Commit

---
+++ SDWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDWebImage
+//
+//  Created by zhangjinfeng on 15/8/6.
+//  Copyright (c) 2015年 changyoushikong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
